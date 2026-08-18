8933a60cf62723803334010f96b92c63e33574ab
Javier Raime Salvatierra <supervisorcgc1@atu.gob.pe>
2026-08-18 16:26:41 -0500

    Agregar script de construccion build.sh

--- mi_sitio/wsgi.py
+++ mi_sitio/wsgi.py
@@ -1,10 +1,11 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Ajuste de rutas estándar para Vercel
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_sitio.settings')
 
 application = get_wsgi_application()
-
-# Esta línea le ayuda a Vercel a encontrar la aplicación correctamente
 app = application
-
